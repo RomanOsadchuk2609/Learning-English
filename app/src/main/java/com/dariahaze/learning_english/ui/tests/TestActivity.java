7cb065e0d0136dfb74964b02213723bdab98c8e9
Roman Osadchuk <osadchuk.roma.26@gmail.com>
2019-05-23 19:52:48 +0300

    created scheduled task for time tracking in random tests

--- app/src/main/java/com/dariahaze/learning_english/ui/tests/TestActivity.java
+++ app/src/main/java/com/dariahaze/learning_english/ui/tests/TestActivity.java
@@ -12,12 +12,14 @@ import android.widget.TextView;
 import com.dariahaze.learning_english.R;
 import com.dariahaze.learning_english.dao.DatabaseAccess;
 import com.dariahaze.learning_english.model.RandomQuestion;
-import com.dariahaze.learning_english.utils.Utils;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -29,12 +31,15 @@ public class TestActivity extends AppCompatActivity {
     timeTV, questionNumberTV;
     private Button buttonNext;
     private int usersAnswer = 0;
+    ScheduledExecutorService scheduler;
+    ScheduledFuture scheduledFuture;
+    private int seconds = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
+        scheduler =  Executors.newSingleThreadScheduledExecutor();
         DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
         databaseAccess.open();
         questionList = databaseAccess.get20RandomQuestions();
@@ -59,6 +64,7 @@ public class TestActivity extends AppCompatActivity {
                     correctAnswers++;
                     correctAnswersTV.setText(correctAnswers+"");
                 }
+                scheduler.shutdown();
                 usersAnswer = 1;
                 showCorrectAnswer(questionList.get(questionIndex));
             }
@@ -71,6 +77,7 @@ public class TestActivity extends AppCompatActivity {
                     correctAnswers++;
                     correctAnswersTV.setText(correctAnswers+"");
                 }
+                scheduler.shutdown();
                 usersAnswer = 2;
                 showCorrectAnswer(questionList.get(questionIndex));
             }
@@ -83,39 +90,82 @@ public class TestActivity extends AppCompatActivity {
                     correctAnswers++;
                     correctAnswersTV.setText(correctAnswers+"");
                 }
+                scheduler.shutdown();
                 usersAnswer = 2;
                 showCorrectAnswer(questionList.get(questionIndex));
             }
         });
 
+
+        System.out.println("PreScheduler: seconds = "+seconds+" "+new Date());
+        scheduledFuture = restartScheduler();
+
         buttonNext = findViewById(R.id.btnNextRandomQuestion);
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (questionIndex < questionList.size()-1){
-                    setDefaultAnswerBackground();
-                    questionIndex++;
-                    answerList.add(usersAnswer);
-                    usersAnswer = 0;
-                    loadQuestion(questionList.get(questionIndex));
-                } else {
-                    buttonNext.setVisibility(View.GONE);
-                    LinearLayout layoutResults = findViewById(R.id.layoutResults);
-                    layoutResults.setVisibility(View.VISIBLE);
-                    RecyclerView recyclerView = findViewById(R.id.usersAnswersRV);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                    UsersAnswersAdapter adapter = new UsersAnswersAdapter(answerList, questionList);
-                    recyclerView.setAdapter(adapter);
-                }
+                moveToNextQuestion();
             }
         });
+    }
 
+    private void moveToNextQuestion(){
+        if (!scheduledFuture.isDone()){
+            scheduler.shutdown();
+        }
 
+        if (questionIndex < questionList.size()-1){
+            setDefaultAnswerBackground();
+            questionIndex++;
+            answerList.add(usersAnswer);
+            usersAnswer = 0;
+            loadQuestion(questionList.get(questionIndex));
+            timeTV.setText(getResources().getString(R.string._00_30));
+            scheduledFuture = restartScheduler();
+        } else {
+            buttonNext.setVisibility(View.GONE);
+            LinearLayout layoutResults = findViewById(R.id.layoutResults);
+            layoutResults.setVisibility(View.VISIBLE);
+            RecyclerView recyclerView = findViewById(R.id.usersAnswersRV);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            UsersAnswersAdapter adapter = new UsersAnswersAdapter(answerList, questionList);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    private ScheduledFuture restartScheduler(){
+        seconds = 30;
+        scheduler =  Executors.newSingleThreadScheduledExecutor();
+        return scheduler.scheduleAtFixedRate
+                (new Runnable() {
+                    public void run() {
+                        seconds--;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                String secondsString = seconds+"";
+                                if (seconds<10){
+                                    secondsString = "0"+secondsString;
+                                }
+                                timeTV.setText("00:"+secondsString);
+                            }
+                        });
+                        System.out.println("Scheduler: seconds = "+seconds+" "+new Date());
+                        if (seconds == 0){
+                            scheduler.shutdown();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    moveToNextQuestion();
+                                }
+                            });
+                        }
+                    }
+                }, 1, 1, TimeUnit.SECONDS);
     }
 
     private void loadQuestion(RandomQuestion question){
         System.out.println(question);
-        timeTV.setText(getResources().getString(R.string._00_30));
         int number = questionIndex+1;
         questionNumberTV.setText(number + "/" + questionList.size());
         questionTV.setText(question.getQuestion());
